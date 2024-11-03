71d5f7cef08854f9424d7a3de044b6ddaa4841af
CatDBB101 <catdbb1000@gmail.com>
2024-11-03 18:51:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const cors = require('cors');
+// const cors = require("cors");
 const database = require("./database.js");
 const account = require("./account.js");
 
 const app = express();
 
-const corsOptions = {
-  origin: '*',
-  credentials: true,
-};
+// const corsOptions = {
+//     origin: "*",
+//     credentials: true,
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*"); //หรือใส่แค่เฉพาะ domain ที่ต้องการได้
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
 
 app.use(cookieParser());
 
@@ -26,9 +34,13 @@ app.get("/api/login", async (req, res) => {
 
     var data = await database.getData("account-database");
 
-	var status = account.matchAccount(data.values, body.username, body.password)
+    var status = account.matchAccount(
+        data.values,
+        body.username,
+        body.password
+    );
 
-	res.cookie("key", status.key);
+    res.cookie("key", status.key);
     res.send(status);
 });
 
@@ -37,17 +49,22 @@ app.put("/api/register", async (req, res) => {
 
     var key = account.generateKey();
 
-	var data = await database.getData("account-database");
+    var data = await database.getData("account-database");
 
     var status = account.confirmCreate(data.values, body.username, key);
-	if (status.status.username) {
-		database.putData("account-database!A:C", [body.username, body.password, key]);
-	}
+    if (status.status.username) {
+        database.putData("account-database!A:C", [
+            body.username,
+            body.password,
+            key,
+        ]);
+    }
 
-	res.cookie("key", key)
+    res.cookie("key", key);
     res.send(status);
 });
 
-app.listen(3000, () => {
-    console.log(`Server is running on port 3000`);
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
