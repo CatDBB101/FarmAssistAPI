a5599339b14c3b5ee9d56dcdd9de308a57ff19a3
CatDBB101 <catdbb1000@gmail.com>
2024-11-07 21:19:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,10 @@ async function createNode(key, node_name) {
 app.post("/api/node/build", async (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
     res.setHeader("Access-Control-Allow-Credentials", true);
 
     var params = req.query;
