b0568c3a34adf9360bbd443d16209af4e924c60c
CatDBB101 <catdbb1000@gmail.com>
2024-11-09 09:50:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5500", credentials: true }));
+app.use(cors({ origin: "127.0.0.1:5500", credentials: true }));
 
 // app.use((req, res, next) => {
 //     res.setHeader("Access-Control-Allow-Origin", "*"); //หรือใส่แค่เฉพาะ domain ที่ต้องการได้
